7210bdba92a3bb1a39cea092506448c0cb12d054
mshwang87 <mshwang87@naver.com>
2022-04-07 22:19:43 +0900

    11

--- src/main/java/socar/Car.java
+++ src/main/java/socar/Car.java
@@ -9,6 +9,8 @@ import java.util.List;
 @Table(name="Car_table")
 public class Car  {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long carId;
 
     private String status;
