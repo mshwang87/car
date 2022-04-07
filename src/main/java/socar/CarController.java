e68536dd2e3d1a79654e0e3ecb51e55ab886fe46
mshwang87 <mshwang87@naver.com>
2022-04-08 08:29:51 +0900

    1

--- src/main/java/socar/CarController.java
+++ src/main/java/socar/CarController.java
@@ -8,19 +8,38 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
 
  @RestController
  public class CarController {
 
-
-
+        @Autowired
+        CarRepository carRepository;
 
 @RequestMapping(value = "/chkAndReqReserve",
         method = RequestMethod.GET,
         produces = "application/json;charset=UTF-8")
 
-public void chkAndReqReserve(HttpServletRequest request, HttpServletResponse response)
+public boolean chkAndReqReserve(HttpServletRequest request, HttpServletResponse response)
         throws Exception {
         System.out.println("##### /car/chkAndReqReserve  called #####");
+
+        // Parameter로 받은 CarID 추출
+        long carId = Long.valueOf(request.getParameter("carId"));
+        System.out.println("######################## chkAndReqReserve carId : " + carId);
+
+        // CarId 데이터 조회
+        Optional<Car> res = carRepository.findById(carId);
+        Car car = res.get(); // 조회한 car 데이터
+        System.out.println("######################## chkAndReqReserve car.getStatus() : " + car.getStatus());
+
+        // car의 상태가 available이면 true
+        boolean result = false;
+        if(car.getStatus().equals("available")) {
+                result = true;
+        } 
+
+        System.out.println("######################## chkAndReqReserve Return : " + result);
+        return result;
         }
  }
